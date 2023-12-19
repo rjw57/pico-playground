f632a5e13f2797308a4aefb968ab6876d92bfba3
Rich Wareham <rich@richwareham.com>
2023-12-19 14:06:12 +0000

    add flashing cursor

--- playground/playground.c
+++ playground/playground.c
@@ -18,6 +18,7 @@ uint8_t *frame_buffer;
 uint width, height, stride;
 
 uint cursor_row, cursor_col;
+bool cursor_shown;
 
 #define console_rows() (height >> 3)
 #define console_cols() (width >> 3)
@@ -26,40 +27,95 @@ void console_reset(void);
 void console_putc(char c);
 void console_line_feed(void);
 void console_carriage_return(void);
+void console_refresh(void);
 
-void console_reset(void) { cursor_row = cursor_col = 0; }
+void console_intl_toggle_cursor(void);
+
+void console_reset(void) {
+  cursor_row = cursor_col = 0;
+  cursor_shown = false;
+}
+
+void console_intl_toggle_cursor(void) {
+  uint8_t *dest = frame_buffer + cursor_col + (6 + (cursor_row << 3)) * stride;
+  for (int i = 0; i < 2; i++, dest += stride) {
+    *dest ^= 0xFF;
+  }
+}
 
 void console_putc(char c) {
-  if ((c >= 32) && (c <= 127)) {
+  bool cursor_was_shown = cursor_shown;
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = false;
+  }
+
+  if ((c >= 32) && (c < 127)) {
     uint8_t *char_rows = font + ((c - 32) << 3);
-    uint8_t *dest = frame_buffer + cursor_col + cursor_row * (stride << 3);
+    uint8_t *dest = frame_buffer + cursor_col + (cursor_row << 3) * stride;
     for (int i = 0; i < 8; i++, char_rows++, dest += stride) {
       *dest = *char_rows;
     }
 
     cursor_col += 1;
-    if(cursor_col >= console_cols()) {
+    if (cursor_col >= console_cols()) {
       console_carriage_return();
       console_line_feed();
     }
-  } else if (c == '\n') {
+  } else if (c == 0x0A) {
     console_line_feed();
-  } else if (c == '\r') {
+  } else if (c == 0x0D) {
     console_carriage_return();
+  }
+
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = true;
   }
 }
 
 void console_carriage_return(void) {
+  bool cursor_was_shown = cursor_shown;
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = false;
+  }
+
   cursor_col = 0;
+
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = true;
+  }
 }
 
 void console_line_feed(void) {
+  bool cursor_was_shown = cursor_shown;
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = false;
+  }
+
   cursor_row += 1;
-  while(cursor_row >= console_rows()) {
+  while (cursor_row >= console_rows()) {
     memmove(frame_buffer, frame_buffer + (stride << 3), (height - 8) * stride);
-    memset(frame_buffer + (height - 8) * stride, 0x00, stride<<3);
+    memset(frame_buffer + (height - 8) * stride, 0x00, stride << 3);
     cursor_row--;
   }
+
+  if (cursor_was_shown) {
+    console_intl_toggle_cursor();
+    cursor_shown = true;
+  }
+}
+
+void console_refresh(void) {
+  static uint frame_count = 0;
+  if (frame_count == 0) {
+    console_intl_toggle_cursor();
+    cursor_shown = !cursor_shown;
+  }
+  frame_count = (frame_count + 1) & 0xf;
 }
 
 int main() {
@@ -74,11 +130,14 @@ int main() {
 
   frame_buffer = malloc(stride * height);
   tvout_set_frame_buffer(frame_buffer);
+  tvout_set_vblank_callback(console_refresh);
 
   tvout_start();
   console_reset();
 
-  memcpy(frame_buffer, family, stride * height);
+  // memcpy(frame_buffer, family, stride * height);
+  memset(frame_buffer, 0x00, stride * height);
+
   while (true) {
     char c = uart_getc(uart0);
     console_putc(c);
